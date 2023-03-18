5ee6d4b920ad6ce4298fa4d1b149e207776f605a
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2023-03-18 10:20:18 +0530

    Create File Manager.py

---
+++ File Manager.py
@@ -0,0 +1,10 @@
+import os
+
+#system window settings 
+os.system("mode con:cols=100 lines=3000");
+os.system("title=File Management System v1.0")
+#system window settings end
+
+
+
+os.system("pause")
