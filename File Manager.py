262ed220f7b3062fa5cc18a1b145ca76e044eefa
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2023-03-18 10:46:03 +0530

    Update File Manager.py

--- File Manager.py
+++ File Manager.py
@@ -17,9 +17,40 @@ def logo():
 	os.system("color 03")
 	os.system("timeout 01>>nul")
 	os.system("color 02")
+	os.system("echo [Current location] %cd%")
+	print()
 
 
+def options():
+	print("[v1.0]Let's see what we can do:")
+	print("\t[1]-List directories")
+	print("\t[2]-Create directory")
+	print("\t[3]-Change directory")
+	print("\t[4]-Rename directory")
+	print("\t[5]-Delete directory")
+	print("\t[6]-Create file")
+	print("\t[7]-Rename file")
+	print("\t[8]-Delete file")
+	print("\t[9]-Write file")
+	print()
+	x=int(input("[?]Select your option :"))
 
+
+def listDir():
+	os.system("cls")
+	print("================================================")
+	print("===================Directories==================")
+	print("================================================")
+	print("================================================")
+	print()
+	print()
+	os.system("dir")
+	print()
+	print("[!]Press any key to go back.")
+	os.system("pause>>nul")
+	logo()
+	options()
 
 logo()
+options()
 os.system("pause")
