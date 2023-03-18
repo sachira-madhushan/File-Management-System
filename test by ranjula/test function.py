5d0b4f851a386f71077586a01a1c14e387b99931
ranjula bandara <ranjulabandara790@gmail.com>
2023-03-18 12:44:33 +0530

    1

---
+++ test by ranjula/test function.py
@@ -0,0 +1,27 @@
+import os
+#system window settings
+
+os.system("mode con:cols=50 lines=30");
+os.system("title=File Management System v1.0")
+os.system("color 02")
+#system window settings end
+
+
+def rename():
+    print("================================================")
+    print("==================Rename Folder=================")
+    print("================================================")
+    print("================================================")
+
+    oldname = input("enter folder name that you want rename ?")
+    newname = input("enter new folder name to that you want to rename?")
+    os.system("ren " + oldname + " " + newname)
+
+def delete_folder():
+    print("================================================")
+    print("==================Delete Folder=================")
+    print("================================================")
+    print("================================================")
+
+    del_filename = input ("enter name that you want to delete :")
+    os.system("rmdir "+del_filename)
