0806a8fc064cf6a942b0bc8651c6dd562642c5f7
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2023-03-18 10:30:30 +0530

    files

--- File Manager.py
+++ File Manager.py
@@ -1,10 +1,25 @@
 import os
 
 #system window settings 
-os.system("mode con:cols=100 lines=3000");
+os.system("mode con:cols=50 lines=30");
 os.system("title=File Management System v1.0")
+os.system("color 02")
 #system window settings end
 
+def logo():
+	os.system("cls")
+	print("================================================")
+	print("===========File Management System v1.0==========")
+	print("================================================")
+	print("================================================")
+	os.system("color 05")
+	os.system("timeout 01>>nul")
+	os.system("color 03")
+	os.system("timeout 01>>nul")
+	os.system("color 02")
 
 
+
+
+logo()
 os.system("pause")
